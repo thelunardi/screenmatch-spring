b3f6a9c74eb4879090090300d4c39c77848d4e11
Alexandre Lunardi <alexandre.lunardi2@gmail.com>
2024-10-27 20:38:55 -0300

    env

---
+++ src/main/java/br/com/alura/screenmatch/util/EnvUtil.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Properties;
+
+public final class EnvUtil {
+    private static Properties props = new Properties();
+
+    public static String getApiKey() {
+        var envFile = Paths.get(".env");
+        try (var inputStream = Files.newInputStream(envFile)) {
+            props.load(inputStream);
+            return props.get("API_KEY").toString();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
